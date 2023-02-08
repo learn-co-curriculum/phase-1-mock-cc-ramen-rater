5d24f1012c1029370c587ab6dbc4db4b34a092aa
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-02-08 11:23:11 -0500

    full solution

--- src/index.js
+++ src/index.js
@@ -1,80 +1,147 @@
 //assigning variables in global to have access to them from anywhere
 const ramenMenu = document.querySelector("#ramen-menu");
-let ramenName = document.getElementsByClassName("name")[0];
-let restaurantName = document.getElementsByClassName("restaurant")[0];
-let rating = document.getElementById("rating-display");
-let comment = document.getElementById("comment-display");
-let eachImg = document.getElementsByClassName("detail-image")[0];
+const eachImg = document.getElementsByClassName("detail-image")[0];
+const ramenName = document.getElementsByClassName("name")[0];
+const restaurantName = document.getElementsByClassName("restaurant")[0];
+const rating = document.getElementById("rating-display");
+const comment = document.getElementById("comment-display");
+const form = document.getElementById('new-ramen')
+const editForm = document.getElementById('edit-ramen')
+const deleteBtn = document.getElementById('delete-btn')
+// let selectedRamen
+let ramenData = []
 
-// getting data from a local server(using fetch)
-//also calling even listener on load to get default ramen loaded
-function fetchRamen() {
-  return fetch("http://localhost:3000/ramens")
-    .then((response) => response.json())
-    .then((data) => {
-      window.addEventListener("load", defaultPage(data));
-      renderRamen(data);
-    })
-    .catch((err) => console.log(err)); //catching an error if there is one
+
+// fetching data from db.json
+// calling function for each ramen that will display images in the menu div
+// calling function that takes the first ramen as an argument
+fetch('http://localhost:3000/ramens')
+.then(res => res.json())
+.then(data => {
+  ramenData = data
+  renderData(data)
+  renderRamen(data[0])
+})
+
+// function loops through all the data
+function renderData(data) {
+  data.forEach(ramen => renderMenu(ramen))
 }
 
-//adding default info on the first load of the page
-function defaultPage(ramen) {
-  eachImg.src = ramen[0].image;
-  ramenName.textContent = ramen[0].name;
-  restaurantName.textContent = ramen[0].restaurant;
-  rating.textContent = ramen[0].rating;
-  comment.textContent = ramen[0].comment;
+// function that displays menu-div
+// adding event listener to each image to display detailed info
+function renderMenu(ramen) {
+  const menuImg = document.createElement('img')
+  menuImg.src = ramen.image
+  ramenMenu.append(menuImg)
+  menuImg.addEventListener('click', () => renderRamen(ramen))
 }
 
-//function that loops each ramen and lists all the menu on top
-function renderRamen(ramenList) {
-  ramenList.map((ramen) => {
-    let img = document.createElement("img");
-    img.src = ramen.image;
-    ramenMenu.appendChild(img);
+// function displays detailed info about each ramen
+function renderRamen(ramen) {
+  // selectedRamen = ramen
+  eachImg.src = ramen.image
+  ramenName.textContent = ramen.name
+  restaurantName.textContent = ramen.restaurantName
+  rating.textContent = ramen.rating
+  comment.textContent = ramen.comment
 
-    //adding event listener to each photo
-    img.addEventListener("click", () => {
-      eachImg.src = ramen.image;
-      ramenName.textContent = ramen.name;
-      restaurantName.textContent = ramen.restaurant;
-      rating.textContent = ramen.rating;
-      comment.textContent = ramen.comment;
-    });
-  });
+  //setting attributes to edit form and delete btn to have access to current ramen's id
+  editForm.setAttribute('ramen-id', ramen.id);
+  deleteBtn.setAttribute('ramen-id', ramen.id);
 }
 
-//submitting the form and creating new ramen
-const form = document.querySelector("#new-ramen");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let newRamenInfo = {
-    name: e.target["new-name"].value,
-    restaurant: e.target["new-restaurant"].value,
-    image: e.target["new-image"].value,
-    rating: e.target["new-rating"].value,
-    comment: e.target["new-comment"].value,
-  };
 
-  createNewRamen(newRamenInfo);
-  form.reset();
-});
+// function runs after user submits the form to add new ramen
+function addNewRamen(e) {
+  e.preventDefault()
+  console.log(e)
+  // console.log(document.querySelector('#new-name').value)
+  // console.log(e.target.name.value)
+  // console.log(e.target[0].value)
 
-//Using fetch and method POST to save data on the server
-function createNewRamen(info) {
-  img.src = info.image;
-  ramenMenu.append(img);
-  fetch("http://localhost:3000/ramens", {
+  // 
+  const newRamen =  {
+    name: e.target.name.value,
+    restaurant: e.target.restaurant.value,
+    image: e.target.image.value,
+    rating: e.target.rating.value,
+    comment: e.target['new-comment'].value
+  }
+
+  // posting new ramen to database
+  fetch('http://localhost:3000/ramens', {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(info),
+    body: JSON.stringify(newRamen)
   })
-    .then((response) => response.json())
-    .then((ramen) => console.log(ramen)); //just logging to see the result
+  .then(res => res.json())
+  .then(data => {
+    // updating the menu div with the new ramen
+    // updating detailed info with the new ramen
+    renderRamen(data)
+    renderMenu(data)
+  })
+
+  // resetting the form
+  e.target.reset()
 }
 
-fetchRamen();
+
+function editRamen(e) {
+  e.preventDefault()
+
+  const ramenId = e.target.getAttribute('ramen-id')
+
+  // rating.textContent = e.target.rating.value
+  // comment.textContent = e.target['new-comment'].value
+  // selectedRamen.rating = e.target.rating.value
+  // selectedRamen.comment = e.target['new-comment'].value
+
+  const updatedRamen = {
+    rating: e.target.rating.value,
+    comment: e.target['new-comment'].value
+  }
+
+  // updating ramen info on our database
+  fetch(`http://localhost:3000/ramens/${ramenId}`, {
+    method: "PATCH",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(updatedRamen)
+  })
+  .then(res => res.json())
+  .then(data => {
+    // updating detailed info with the updated ramen
+    renderRamen(data)
+  })
+
+  // resetting the form
+  e.target.reset()
+}
+
+function deleteRamen(e) {
+
+  const ramenId = e.target.getAttribute('ramen-id')
+
+  fetch(`http://localhost:3000/ramens/${ramenId}`, {
+    method: "DELETE"
+  })
+  .then(res => res.json())
+  .then(() => {
+    // filter through ramenData array and return new array without deleted ramen
+    const updatedData = ramenData.filter(ramen => ramen.id !== ramenId)
+    renderData(updatedData)
+    renderRamen(updatedData[0])
+  })
+}
+
+
+//Event listeners
+
+form.addEventListener('submit', addNewRamen)
+editForm.addEventListener('submit', editRamen)
+deleteBtn.addEventListener('click', deleteRamen)
