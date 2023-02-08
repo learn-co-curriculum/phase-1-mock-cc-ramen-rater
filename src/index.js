a79ad36c9de92979e58707e3a6aa293fa72839bf
Michael Gioeni <gioenijr@oswego.edu>
2023-02-08 15:42:53 -0700

    fixed after walkthru

--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
-// write your code here
-const menuAPI = "http://localhost:3000";
-document.querySelector('#new-ramen').addEventListener('submit', handleSubmit)
+const apiUrl = 'http://localhost:3000/ramens'
+const ramenMenuDiv = document.getElementById('ramen-menu')
+const newRamenForm = el('new-ramen')
+el('new-ramen').addEventListener('submit',newRamenHandler)
+fetch(apiUrl)
+.then(response => response.json())
+.then(renderRamens)
 
-function handleSubmit(e){
-    e.preventDefault()
-    let ramenObj={
-        name:e.target.name.value,
-        imageURL:e.target.image_url.value,
-        restaurant:e.target.restaurant.value,
-        rating:e.target.rating.value,
-        comment:e.target.comment.value,
+function renderRamens(ramens){
+    ramens.forEach(renderRamen);
+}
+function renderRamen(ramen){
+    const ramenImageElement = document.createElement('img');
+    ramenImageElement.src= ramen.image;
+    ramenMenuDiv.append(ramenImageElement);
+
+    ramenImageElement.addEventListener('click' ,() => ramenClickHandler(ramen));
+}
+function ramenClickHandler(ramen){
+    el('detail-image').src = ramen.image;
+    el('detail-name').textContent = ramen.name;
+    el('detail-restaurant').textContent=ramen.rating;
+    el('rating-display').textContent=ramen.rating
+    el('comment-display').textContent = ramen.comment;
+
+}
+function el(id){
+    return document.getElementById(id);
+}
+function newRamenHandler(e){
+    e.preventDefault();
+
+    const newRamen = {
+        name: e.target.name.value,
+        restaurant: e.target.restaurant.value,
+        image: e.target.image.value,
+        rating: e.target.rating.value,
+        comment: e.target['new-comment'].value,
     }
-
-renderOneRamen(ramenObj)
-newRamen(ramenObj)
-}
-
-//render functions
-function renderOneRamen(ramen){
-    //build ramen
-    let menu =getElementById('#ramen-menu')
-    menu.createElement('li')
-    menu.className= 'menu'
-    menu.innerHTML = 
-    `<img src=${ramen.image}>
-    //<h3>${ramen.name}</h3>
-    `
-}
-//add ramen card to the DOM
-document.querySelector('#ramen-menu').appendChild(menu)
-
-
-function newRamen(){
-fetch(menuAPI)
-.then(res => res.json())
-.then(menuData => menuData.forEach(ramen =>renderOneRamen(ramen)))
+    renderRamen(newRamen);
+    e.target.reset();
 
 }
