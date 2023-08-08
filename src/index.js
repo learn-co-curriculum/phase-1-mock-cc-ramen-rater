a3105bc105789b02a956888eedb6f5344eca8cbf
Jason <jconn99801@gmail.com>
2023-08-08 12:48:54 -0800

    trying new process

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-fetch("http://localhost:3000")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-.catch(error => console.log('ERROR'));
+const url = "http://localhost:3000";
+
+//* Fetch Data & Call render functions to populate the DOM
+getJSON(url + "/ramens").then((ramens) => {
+  renderRamenReviewCard(ramens[0]);
+});
+
+//* Render Functions
+function renderRamenReviewCard(ramenReview) {
+  document.querySelector("h2.name").textContent = ramenReview.name;
+  document.querySelector("h3.restaurant").textContent = ramenReview.restaurant;
+  document.querySelector("#ramen-image").src = ramenReview.image;
+  document.querySelector("#rating-display").textContent = ramenReview.rating;
+  document.querySelector("#comment-display").textContent = ramenReview.comment;
+}
+
+// function renderRamenMenu(ramenMenu) {
+//     forEach(ramenMenu) {
+
+//     }
+// }
