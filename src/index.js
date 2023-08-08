c7560f5d84d6f3d2743bdb0ad9b8fffda2959ff6
Jason <jconn99801@gmail.com>
2023-08-08 13:40:50 -0800

    added render functions

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ const ramenForm = document.querySelector("#new-ramen");
 
 const url = "http://localhost:3000";
 
-getJSON(url + "/ramens").then((ramens) => {});
+getJSON(url + "/ramens").then((ramens) => {
+  ramens.forEach((ramen) => renderMenu(ramen));
+});
 
 //* Render Functions
 
-function renderMenu() {}
-function renderMenuDetails() {}
+function renderMenu(ramen) {
+  const img = document.createElement("img");
+  img.src = ramen.image;
+  img.alt = `Picture of ${ramen.name}`;
+  ramenMenu.appendChild(img);
+}
+function renderMenuDetails(ramen) {
+  //* put selected ramen on page
+}
