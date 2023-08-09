c54cf250c695772c329c5de370edba62b9683e2f
Jason <jconn99801@gmail.com>
2023-08-08 22:00:23 -0800

    "cleaned up index.js"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 const url = "http://localhost:3000";
 
-//*GIMME DAT DATA
-getJSON(url + "/ramens").then((ramens) => {
-  ramens.forEach(renderMenu);
-});
-
 //* HTML Selectors
 const ramenDetails = document.querySelector("#ramen-details");
 const ramenForm = document.querySelector("#new-ramen");
@@ -13,10 +8,15 @@ const ramenDetailsRestaurant = document.querySelector("h3.restaurant");
 const ramenDetailsImage = document.querySelector("img");
 const ramenDetailsRating = document.querySelector("#rating-display");
 const ramenDetailsComment = document.querySelector("#comment-display");
+const ramenMenu = document.querySelector("#ramen-menu");
+
+//*GIMME DAT DATA
+getJSON(url + "/ramens").then((ramens) => {
+  ramens.forEach(renderMenu);
+});
 
 //* Render Functions
 const renderMenu = (ramen) => {
-  const menu = document.querySelector("#ramen-menu");
   const img = document.createElement("img");
 
   img.id = `ramenImage-${ramen.id}`;
@@ -27,14 +27,15 @@ const renderMenu = (ramen) => {
   img.dataset.restaurant = ramen.restaurant;
   img.dataset.rating = ramen.rating;
   img.dataset.comment = ramen.comment;
+  img.dataset.name = ramen.name;
 
   img.addEventListener("click", function () {
-    ramenDetailsName.textContent = this.alt;
+    ramenDetailsName.textContent = this.dataset.name;
     ramenDetailsImage.src = this.src;
     ramenDetailsComment.textContent = this.dataset.comment;
     ramenDetailsRating.textContent = this.dataset.rating;
     ramenDetailsRestaurant.textContent = this.dataset.restaurant;
   });
 
-  menu.append(img);
+  ramenMenu.append(img);
 };
