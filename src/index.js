e1bc5a42c615da831ddb50edbc424222bf9c86b1
Jason <jconn99801@gmail.com>
2023-08-09 08:38:23 -0800

    made renderDetails into two separate functions.

--- src/index.js
+++ src/index.js
@@ -11,31 +11,27 @@ const ramenDetailsComment = document.querySelector("#comment-display");
 const ramenMenu = document.querySelector("#ramen-menu");
 
 //*GIMME DAT DATA
+
 getJSON(url + "/ramens").then((ramens) => {
   ramens.forEach(renderMenu);
+  renderDetails(ramens[0]);
 });
 
 //* Render Functions
+const renderDetails = (ramen) => {
+  ramenDetailsName.textContent = ramen.name;
+  ramenDetailsImage.src = ramen.image;
+  ramenDetailsComment.textContent = ramen.comment;
+  ramenDetailsRating.textContent = ramen.rating;
+  ramenDetailsRestaurant.textContent = ramen.restaurant;
+};
+
 const renderMenu = (ramen) => {
   const img = document.createElement("img");
-
   img.id = `ramenImage-${ramen.id}`;
   img.className = "menu-Images";
   img.src = ramen.image;
   img.alt = `Picture of ${ramen.name}`;
-  img.dataset.id = ramen.id;
-  img.dataset.restaurant = ramen.restaurant;
-  img.dataset.rating = ramen.rating;
-  img.dataset.comment = ramen.comment;
-  img.dataset.name = ramen.name;
-
-  img.addEventListener("click", function () {
-    ramenDetailsName.textContent = this.dataset.name;
-    ramenDetailsImage.src = this.src;
-    ramenDetailsComment.textContent = this.dataset.comment;
-    ramenDetailsRating.textContent = this.dataset.rating;
-    ramenDetailsRestaurant.textContent = this.dataset.restaurant;
-  });
-
+  img.addEventListener("click", renderDetails(ramen));
   ramenMenu.append(img);
 };
